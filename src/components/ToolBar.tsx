c91d7b21e1037afaf862f7e948c2f9f18ce68968
Chad Christensen <chadchristensen2@gmail.com>
2020-02-06 11:01:37 -0500

    Refactor class for buttons with icons

--- src/components/ToolBar.tsx
+++ src/components/ToolBar.tsx
@@ -11,14 +11,18 @@ const ToolBar: React.FC<ToolBarProps> = ({ tool, handleToolChange }) => {
   return (
     <div className="toolbar">
       <button
-        className={tool === Tools.Paint ? 'selected' : ''}
+        className={['btn-with-icon']
+          .concat(tool === Tools.Paint ? 'selected' : '')
+          .join(' ')}
         onClick={() => handleToolChange(Tools.Paint)}
       >
         <FontAwesomeIcon icon={faPaintBrush} />
         <span>{Tools.Paint.toLowerCase()}</span>
       </button>
       <button
-        className={tool === Tools.Erase ? 'selected' : ''}
+        className={['btn-with-icon']
+          .concat(tool === Tools.Erase ? 'selected' : '')
+          .join(' ')}
         onClick={() => handleToolChange(Tools.Erase)}
       >
         <FontAwesomeIcon icon={faEraser} />
